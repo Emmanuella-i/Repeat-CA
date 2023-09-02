3872296e506ca7f6e4f7b71d52083c0a8530177d
Emmanuella-i <113349379+Emmanuella-i@users.noreply.github.com>
2023-09-02 01:03:49 +0100

    Added Menu

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -8,28 +8,85 @@ import DAO.IEmployeeDAOInterface;
 import DAO.MysqlEmployeeDAO;
 import DTO.Employee;
 import Exceptions.DAOException;
+import java.util.Scanner;
 
 import java.util.List;
 public class App 
 {
+    private static Scanner keyboard = new Scanner(System.in);
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
         IEmployeeDAOInterface employeeDAO = new MysqlEmployeeDAO();
-        findAndPrintAllEmployees(employeeDAO);
-        findAndPrintEmployeeById(employeeDAO, "80");
-        deleteEmployeeById(employeeDAO, "1");
-        //insertEmployee(employeeDAO, );
-        findAllEmployeesJSON(employeeDAO);
-        findEmployeeByIdJSON(employeeDAO, "80");
+
+        System.out.println("Organisation Employee Database");
+        Options selectedOption = Options.PRINT_MENU;
+
+        boolean quit = false;
+        printOptions();
+
+        while (selectedOption != Options.QUIT) {
+            System.out.print("\n Enter action: (0 to show available options) ");
+            selectedOption = Options.values()[Integer.parseInt(keyboard.nextLine().trim())];
+            switch (selectedOption) {
+                case PRINT_MENU:
+                    printOptions();
+                    break;
+                case FIND_AND_DISPLAY_ALL_EMPLOYEES:
+                    findAndPrintAllEmployees(employeeDAO);
+                    break;
+                case FIND_EMPLOYEE_BY_ID:
+                    findAndPrintEmployeeById(employeeDAO);
+                    break;
+                case FIND_AND_DISPLAY_ALL_EMPLOYEES_JSON:
+                    findAllEmployeesJSON(employeeDAO);
+                    break;
+                case FIND_EMPLOYEE_BY_ID_JSON:
+                    findEmployeeByIdJSON(employeeDAO);
+                    break;
+                case DELETE_EMPLOYEE_BY_ID:
+                    deleteEmployeeById(employeeDAO);
+                    break;
+                case ADD_EMPLOYEE:
+
+                    insertEmployee(employeeDAO);
+                    break;
+                case QUIT:
+                    System.out.println("Shutting down simulator....");
+                    quit = true;
+                    break;
+
+            }
+        }
+
+
+
+
+
+
+
     }
 
-    private static void findEmployeeByIdJSON(IEmployeeDAOInterface employeeDAO, String number)
+    private static void printOptions()
+    {
+        System.out.println("\n Available options: ");
+        System.out.println("0 - to print all options \n"+
+                "1 -  Display all employees\n" +
+                "2 -  Find Employee By ID\n" +
+                "3 -  JSON Display all Employees\n" +
+                "4 -  JSON Find Employee By ID\n" +
+                "5 -  Delete Employee By ID\n" +
+                "6 -  Add New Employee\n" +
+                "7 - to quit");
+    }
+
+    private static void findEmployeeByIdJSON(IEmployeeDAOInterface employeeDAO)
     {
         try
         {
+            System.out.println("Input ID: ");
+            String id = keyboard.nextLine();
 
-            String employee = employeeDAO.findEmployeeByIdJson(number);
+            String employee = employeeDAO.findEmployeeByIdJson(id);
             System.out.println(employee);
 
         }
@@ -56,11 +113,13 @@ public class App
         }
     }
 
-    private static void findAndPrintEmployeeById(IEmployeeDAOInterface employeeDAO, String employeeId)
+    private static void findAndPrintEmployeeById(IEmployeeDAOInterface employeeDAO)
     {
         try
         {
-            Employee employee = employeeDAO.findEmployeeByID(employeeId);
+            System.out.println("Input ID: ");
+            String id = keyboard.nextLine();
+            Employee employee = employeeDAO.findEmployeeByID(id);
             System.out.println(employee);
         }
         catch (DAOException e)
@@ -86,11 +145,13 @@ public class App
         }
     }
 
-    private static void deleteEmployeeById(IEmployeeDAOInterface employeeDAO, String employeeId)
+    private static void deleteEmployeeById(IEmployeeDAOInterface employeeDAO)
     {
         try
         {
-            employeeDAO.deleteEmployeeByID(employeeId);
+            System.out.println("Input ID: ");
+            String id = keyboard.nextLine();
+            employeeDAO.deleteEmployeeByID(id);
         }
         catch (DAOException e)
         {
@@ -98,10 +159,26 @@ public class App
         }
     }
 
-    private static void insertEmployee(IEmployeeDAOInterface employeeDAO, Employee employee)
+    private static void insertEmployee(IEmployeeDAOInterface employeeDAO)
     {
         try
         {
+
+            System.out.println("Type name: ");
+            String name = keyboard.nextLine();
+
+            System.out.println("Type ID: ");
+            int id = keyboard.nextInt();
+
+
+
+            System.out.println("Type age: ");
+            int age = keyboard.nextInt();
+
+            System.out.println("Type hours worked: ");
+            float hours = keyboard.nextFloat();
+
+            Employee employee = new Employee(id, name, age, hours);
             employeeDAO.insertEmployee(employee);
         }
         catch (DAOException e)
